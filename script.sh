66898582e266a8b091133a6c1acc585e2ac07c7d
DamienDeberthe <deberthedamien92@gmail.com>
2025-03-17 22:21:43 +0100

    Add

--- script.sh
+++ script.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+git status --porcelain
+
+echo ""
+echo ""
+
 if git status --porcelain | grep -q .; then
-  echo "Il y a des changements."
-else
-  echo "Aucun changement."
+  if git status --porcelain | awk '$1 ~ /M/ { found=1 } END { exit !found }'; then
+    echo "Il y a des changements."
+  fi
 fi
 
 
