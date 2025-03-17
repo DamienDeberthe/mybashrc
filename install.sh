7a4f781a83dab90588823e97c5262bde9dfaa5f9
DamienDeberthe <deberthedamien92@gmail.com>
2025-03-17 23:53:54 +0100

    Fix/Add download mybashrc

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 
 if ! grep -q "Installed with 'mybashrc'" ~/.bashrc; then
   echo -e "\n" >> ~/.bashrc
+  wget -q -O mybashrc  https://raw.githubusercontent.com/DamienDeberthe/mybashrc/main/mybashrc 
   cat mybashrc >> ~/.bashrc
+  rm mybashrc
   echo "'mybashrc' has been installed."
 else
   echo "'mybashrc' already installed for current user."
